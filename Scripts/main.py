6d7edde8730df25c47ad96c3e0df1ca40d6b7ec2
umerjillani <umerjillani1@gmail.com>
2025-06-20 21:21:15 +0500

    Removed validator functionality

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,10 +1,7 @@
-# total of all entities is not being fetched in json 
-# fetch / calculate total of all gross pays 
-
 from ocr import perform_OCR
 from json_creation import create_JSON
 from json_creation import REQUIRED_KEYS
-from validator import validate_amounts
+# from validator import validate_amounts 
 from pathlib import Path
 
 
@@ -17,4 +14,4 @@ if __name__ == "__main__":
     
     perform_OCR(input_folder, output_folder)  
     create_JSON(output_folder, REQUIRED_KEYS, json_folder)
-    validate_amounts(json_folder) 
+    # validate_amounts(json_folder) 
